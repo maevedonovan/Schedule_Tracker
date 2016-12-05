b0fb632cdcb155becefc0070811a39d674477325
“squirrelgirl58” <kdillon2@nd.edu>
2016-12-05 17:25:00 -0500

    more method

--- schedule_tracker/app/models/course.rb
+++ schedule_tracker/app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   has_many :students, through: Registration
   belongs_to :professor
+
+  def current_enroll
+    
 end
--- schedule_tracker/app/models/professor.rb
+++ schedule_tracker/app/models/professor.rb
@@ -1,3 +1,8 @@
 class Professor < ApplicationRecord
   has_many :courses
+
+  def course_load
+    @professor.courses.count 
+  end
+
 end
