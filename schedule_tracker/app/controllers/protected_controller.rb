8210e2e5f99f532c69a9504d729876037b59a6a9
maevedonovan <mdonova8@nd.edu>
2016-12-06 12:15:55 -0500

    login/logout path update

--- schedule_tracker/app/controllers/protected_controller.rb
+++ schedule_tracker/app/controllers/protected_controller.rb
@@ -1,2 +1,11 @@
 class ProtectedController < ApplicationController
+
+  before_action :require_login
+
+  def require_login
+    unless session[:net_id]
+      flash[:message] = "Access Denied."
+      redirect_to login_path
+    end
+  end
 end
